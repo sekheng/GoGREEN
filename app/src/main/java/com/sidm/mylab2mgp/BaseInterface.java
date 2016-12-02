2bd9be872bba3d07cf9a4bc87919e167de4349a5
unknown <150629Z@M615G204.sidm.student.local>
2016-12-02 14:31:10 +0800

    Trying to recreate ECS Architecture.

---
+++ app/src/main/java/com/sidm/mylab2mgp/BaseInterface.java
@@ -0,0 +1,10 @@
+package com.sidm.mylab2mgp;
+
+/**
+ * Created by 150629Z on 12/2/2016.
+ */
+
+public interface BaseInterface {
+    public void Update(long dt);
+    public void Exit();
+}
---
+++ app/src/main/java/com/sidm/mylab2mgp/Component.java
@@ -0,0 +1,21 @@
+package com.sidm.mylab2mgp;
+
+/**
+ * Created by 150629Z on 12/2/2016.
+ */
+
+public class Component implements BaseInterface {
+    Component()
+    {
+        name_ = "";
+    }
+    public void Update(long dt)
+    {
+
+    }
+    public void Exit()
+    {
+
+    }
+    protected String name_;
+}
