4fd9db94c90879c4516430fea5c5fe8a15afa7e0
Ah Heng <leesekheng@yahoo.com>
2016-12-07 21:40:16 +0800

    Changed some stuff to make animation easier.

--- app/src/main/java/ECS/BitComponent.java
+++ app/src/main/java/ECS/BitComponent.java
@@ -2,6 +2,7 @@ package ECS;
 
 import android.graphics.Bitmap;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
 /**
@@ -12,18 +13,28 @@ public class BitComponent extends Component {
     public BitComponent()
     {
         name_ = "Ze Images";
-        allTheImages = new LinkedList<Bitmap>();
+        //allTheImages = new LinkedList<Bitmap>();
+        allTheImages = new HashMap<String, Bitmap>();
         HistoryOfSprite = new LinkedList<Bitmap>();
     }
     public void setImages(Bitmap zeImage)
     {
         if (HistoryOfSprite.isEmpty())
             HistoryOfSprite.add(zeImage);
-        allTheImages.add(zeImage);
+        //allTheImages.add(zeImage);
+        allTheImages.put(Long.toString(num++), zeImage);
+    }
+    public void setImages(Bitmap zeImage, String zeName)
+    {
+        if (HistoryOfSprite.isEmpty())
+            HistoryOfSprite.add(zeImage);
+        allTheImages.put(zeName, zeImage);
     }
    public Bitmap getCurrImage()
     {
         return  HistoryOfSprite.getLast();
     }
-    public LinkedList<Bitmap> allTheImages, HistoryOfSprite;
+    public LinkedList<Bitmap> /*allTheImages,*/ HistoryOfSprite;
+    HashMap<String, Bitmap> allTheImages;
+    private static long num = 0;
 }
